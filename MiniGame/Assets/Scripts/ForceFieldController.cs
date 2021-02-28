b9849058fde53efe23e7e8d70140ea164fd492a6
petrarubic <prubic.dev@gmail.com>
2021-02-28 13:40:57 +0100

    setup third world, removed player prefab from all scenes

---
+++ MiniGame/Assets/Scripts/ForceFieldController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFieldController : TAction
+{
+    public override void OnTrigger(Trigger newTrigger)
+    {
+        Destroy(this.gameObject);
+    }
+}
