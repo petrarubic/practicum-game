d76a59e745136af2cb257e50971ba8050bdce57c
petrarubic <prubic.dev@gmail.com>
2021-02-28 10:48:47 +0100

    setup first two worlds to portal activation

---
+++ MiniGame/Assets/Scripts/PortalController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PortalController : TAction
+{
+    private void Awake()
+    {
+        this.GetComponent<VisualEffect>().Stop();
+    }
+
+    public override void OnTrigger(Trigger newTrigger)
+    {
+        this.GetComponent<VisualEffect>().Play();
+    }
+}
---
+++ MiniGame/Assets/Scripts/PortalOpener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalOpener : Trigger
+{
+    [SerializeField]
+    GameObject portalSphere;
+
+    [SerializeField]
+    GameObject spherePosition;
+
+    public override void ResetTrigger()
+    {
+        
+    }
+
+    public void SpawnPortalSphere()
+    {
+        Instantiate(portalSphere, spherePosition.transform);
+    }
+}
